be509290be39545a92c9649888e939704d51214d
vallaripadole16 <vallaripadole16@gmail.com>
2021-08-30 21:20:22 +0530

    Recycler View added

--- app/src/main/java/com/example/android/chatapplication/MainActivity.kt
+++ app/src/main/java/com/example/android/chatapplication/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.android.chatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userRecyclerView : RecyclerView
+    private lateinit var userList : ArrayList<HomeActivity>
+    private lateinit var adapter: UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userList = ArrayList()
+        adapter = UserAdapter(this,userList)
     }
 }
--- app/src/main/java/com/example/android/chatapplication/SignupActivity.kt
+++ app/src/main/java/com/example/android/chatapplication/SignupActivity.kt
@@ -8,6 +8,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 
 class SignupActivity : AppCompatActivity() {
 
@@ -15,7 +18,7 @@ class SignupActivity : AppCompatActivity() {
     private lateinit var edtEmail : EditText
     private lateinit var edtPassword : EditText
     private lateinit var btnSignup : Button
-
+    private lateinit var mDatabaseRef:DatabaseReference
     private lateinit var mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,24 +33,31 @@ class SignupActivity : AppCompatActivity() {
         btnSignup = findViewById(R.id.btn_signup)
 
         btnSignup.setOnClickListener{
+            val name = edtName.text.toString()
             val email = edtEmail.text.toString()
             val password = edtPassword.text.toString()
 
-            signup(email,password)
+            signup(name,email,password)
         }
     }
 
-    private fun signup(email: String, password: String) {
+    private fun signup(name:String,email: String, password: String) {
     //logic for signing up the user
         mAuth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
                     //code for jumping to home activity
+                        addUserToDatabase(name, email, mAuth.currentUser?.uid!!)
                     val intent = Intent(this,MainActivity::class.java)
                     startActivity(intent)
                 } else {
                     Toast.makeText(this@SignupActivity,"Some error occurred",Toast.LENGTH_SHORT).show()
                 }
             }
+    }
+
+    private fun addUserToDatabase(name: String, email: String, uid: String) {
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference()
+        mDatabaseRef.child("user").child(uid).setValue(HomeActivity(name,email,uid))
     }
 }
