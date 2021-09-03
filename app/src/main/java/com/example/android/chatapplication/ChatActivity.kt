5e5ae54ad1cfbe9472a95e629ed2264d5c7477c5
vallaripadole16 <vallaripadole16@gmail.com>
2021-09-03 20:29:51 +0530

    changes made in UserAdapter, built ChatActivity

--- app/src/main/java/com/example/android/chatapplication/ChatActivity.kt
+++ app/src/main/java/com/example/android/chatapplication/ChatActivity.kt
@@ -1,11 +1,30 @@
 package com.example.android.chatapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
 
 class ChatActivity : AppCompatActivity() {
+
+    private lateinit var chatRecyclerView : RecyclerView
+    private lateinit var messageBox : EditText
+    private lateinit var sendButton : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        val intent = Intent()
+        val name = intent.getStringExtra("name")
+        val uid = intent.getStringExtra("uid")
+
+        supportActionBar?.title = name
+
+        chatRecyclerView = findViewById(R.id.chatRecyclerView)
+        messageBox = findViewById(R.id.messageBox)
+        sendButton = findViewById(R.id.sendButton)
     }
 }
--- app/src/main/java/com/example/android/chatapplication/UserAdapter.kt
+++ app/src/main/java/com/example/android/chatapplication/UserAdapter.kt
@@ -1,11 +1,13 @@
 package com.example.android.chatapplication
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 import java.util.ArrayList
 
 class UserAdapter(val context: Context, val userList:ArrayList<HomeActivity>):RecyclerView.Adapter<UserAdapter.UserViewHolder>() {
@@ -18,6 +20,13 @@ class UserAdapter(val context: Context, val userList:ArrayList<HomeActivity>):Re
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
         val currUser = userList[position]
         holder.textName.text = currUser.name
+
+        holder.itemView.setOnClickListener{
+            val intent = Intent(context,ChatActivity::class.java)
+            intent.putExtra("name",currUser.name)
+            intent.putExtra("uid",FirebaseAuth.getInstance().currentUser?.uid)
+            context.startActivity(intent)
+        }
     }
 
     override fun getItemCount(): Int {
